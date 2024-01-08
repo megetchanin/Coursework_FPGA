60e836edce047a659696ef13838b2ea47cd8e501
Danil <dan.skrebenkov@bk.ru>
2024-01-08 15:13:42 +0300

    source files are added

---
+++ Coursework.srcs/sources_1/new/UART.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 19.12.2023 21:31:10
+// Design Name: 
+// Module Name: UART
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module uart
+
+                
+endmodule
